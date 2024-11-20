a4b870d594dc595427f346749a7d7933fe82c43b
admin <admin@hots.com>
2024-11-20 23:23:53 +0700

    MK0 template update 1

---
+++ MK0/Template/src/config/labhau/labhau_cfg.h
@@ -0,0 +1,11 @@
+#ifndef LABHAU_CFG_H
+#define	LABHAU_CFG_H
+/* ********************************************************** BOARD SELECTION */
+#define USE_BOARD_MK0 // use PCB V0 and PIC32MK1024MCM064 MCU
+//#define USE_BOARD_MK1 // use PCB V1 and PIC32MK1024MCM064 MCU
+//#define USE_BOARD_AK1 // use PCB V1 and dsPIC33AK128MC106 MCU
+//#define USE_BOARD_E71 // use PCB V1 and ATSAME70J20 MCU
+/* ********************************************************** MOTOR SELECTION */
+#define USE_CUSTOM_MOTOR // the motor parameters are defined in labhau_cfg.c
+
+#endif
